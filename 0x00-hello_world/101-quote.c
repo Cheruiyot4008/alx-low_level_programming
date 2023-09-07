5f4ded2aa2417cef6ba8840435c462b58349eb0a
Cheruiyot4008 <Cheruiyotbrian454@gmail.com>
2023-09-07 06:31:26 -0700

    shell last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
-
 /**
  * main - Entry point
  * 
- * Return: Always 1 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
         puts("and that piece of art is useful" - Dora Korpar, 2015-10-19")
-		return (1);
-
+		return (0);
+}
