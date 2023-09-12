6dba695d15f03bf1486da0a1bcd813fe286d9ed1
Cheruiyot4008 <Cheruiyotbrian454@gmail.com>
2023-09-12 10:42:18 -0700

    c hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,10 @@
 #include "stdio.h"
 
 /**
- * jack_bauer - Check stdio
- * Description: function prints every minute in one day
- * Return: Nothing
+ * jack_bauer - Print every minute in one day
+ *
+ * Description: This function prints every minute in a 24-hour day
+ *              in the format HH:MM.
  */
 void jack_bauer(void)
 {
@@ -13,12 +14,12 @@ void jack_bauer(void)
 	{
 		for (M = 0; M < 60; M++)
 		{
-			 putchar((H / 10) + '0');
-			 putchar((H % 10) + '0');
-			 putchar(':');
-			 putchar((M / 10) + '0');
-			 putchar((M % 10) + '0');
-			 putchar('\n');
+			putchar((H / 10) + '0');
+			putchar((H % 10) + '0');
+			putchar(':');
+			putchar((M / 10) + '0');
+			putchar((M % 10) + '0');
+			putchar('\n');
 		}
 	}
 }
