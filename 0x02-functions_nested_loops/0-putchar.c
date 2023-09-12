8f2dda88d5f6b76bfbac94ef5e61eb519e5d1686
Cheruiyot4008 <Cheruiyotbrian454@gmail.com>
2023-09-12 09:58:07 -0700

    c functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,16 +2,16 @@
 
 /**
  * main - Check description
- * Description: It prints the word School, followed by a new line.
+ * Description: It prints the word school, followed by a new line.
  * Return: 0.
  */
 int main(void)
 {
-	char word[9] = "School";
+	char word[9] = "school";
 	int i;
 
 	for (i = 0; i < 9; i++)
-	putchar(word[i]);
+		putchar(word[i]);
 	putchar('\n');
 
 	return (0);
