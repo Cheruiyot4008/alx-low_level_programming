276b03084094cb223304f5414e94bb702ff941ec
Cheruiyot4008 <Cheruiyotbrian454@gmail.com>
2023-09-12 10:36:59 -0700

    corrected sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,31 +1,28 @@
-#include "stdio.h"
+#include <stdio.h>
 
 /**
- * print_sign - Check stdio
- * @n: An input number
- * Description: Tohis function prints the sign of the number
- * Return: 1 if number is positive, o if number is 0, or
- * -1 if number is negative
+ * print_sign - Prints the sign of a number.
+ * @n: The number to check.
+ *
+ * Return: 1 and prints + if n is greater than zero,
+ *         0 and prints 0 if n is zero,
+ *        -1 and prints - if n is less than zero.
  */
 int print_sign(int n)
 {
-	int value;
-
 	if (n > 0)
 	{
-		value = 1;
-		 putchar('+');
+		printf("+\n");
+		return (1);
 	}
 	else if (n == 0)
 	{
-		value = 0;
-		 putchar('0');
+		printf("0\n");
+		return (0);
 	}
 	else
 	{
-		value = -1;
-		putchar('-');
+		printf("-\n");
+		return (-1);
 	}
-
-	return (value);
 }
