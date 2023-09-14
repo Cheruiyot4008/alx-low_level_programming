9c0e151fbb4b0140f1a3b590bc5d24bf4589f6c9
Cheruiyot4008 <Cheruiyotbrian454@gmail.com>
2023-09-14 09:38:05 -0700

    multiple mains

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,28 +1,47 @@
-#include "stdio.h"
+#include <stdio.h>
 
 /**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
+ * largest_number - Find the largest among three integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ * @c: The third integer.
+ *
+ * Return: The largest integer among a, b, and c.
  */
 int largest_number(int a, int b, int c)
 {
 	int largest;
 
 	if (a >= b && a >= c)
-	{
 		largest = a;
-	}
 	else if (b >= a && b >= c)
-	{
 		largest = b;
-	}
-	else if (c >= a && c >= b)
-	{
+	else
 		largest = c;
-	}
 
 	return (largest);
+}
+
+/**
+ * main - Entry point of the program
+ *
+ * Description:
+ * This program demonstrates the use of the largest_number function
+ * by finding the largest number among three integers and printing the result.
+ *
+ * Return: 0 on successful execution
+ */
+int main(void)
+{
+	int a, b, c, largest;
+
+	a = 10;
+	b = 5;
+	c = 20;
+
+	largest = largest_number(a, b, c);
+
+	printf("The largest number among %d, %d, and %d is %d\n", a, b, c, largest);
+
+	return (0);
 }
