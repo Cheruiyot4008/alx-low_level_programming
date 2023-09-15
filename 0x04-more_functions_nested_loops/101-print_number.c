732e5852c150ed651db20c4235ac93432cf33130
Cheruiyot4008 <Cheruiyotbrian454@gmail.com>
2023-09-15 12:25:32 -0700

    task four

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,11 +8,10 @@ void print_integer(int m);
  */
 void print_integer(int m)
 {
-	int i = 1000000000;
+	unsigned int i = m ;
 
-	for (; i >= 1; i /= 10)
-		if (m / i != 0)
-		{
-			_putchar((m / i) % 10 + '0');
+	if (i < m)
+	putchar 
+	{
 		}
 }
