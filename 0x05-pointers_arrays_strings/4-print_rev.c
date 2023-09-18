632cf9481abd433c0109ad90c5f3db4cca909adb
Cheruiyot4008 <Cheruiyotbrian454@gmail.com>
2023-09-18 05:38:55 -0700

    fouth task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+/**
+ * print_rev - Prints a string in reverse followed by a new line.
+ * @s: The string to be printed in reverse.
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+    int length = 0;
+    int i; 
+
+    while (s[length] != '\0')
+        length++;
+
+    for (i = length - 1; i >= 0; i--)
+	    write(1, &s[i], 1);
+
+    write(1, "\n", 1);
+}
