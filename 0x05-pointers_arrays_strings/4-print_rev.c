f17de4f3cfdf3ce87df64a8cbcb77df90692a766
Cheruiyot4008 <Cheruiyotbrian454@gmail.com>
2023-09-18 10:08:42 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,13 @@ int _putchar(char c);
  */
 void print_rev(char *s)
 {
-    int len = 0;
+	int len = 0;
 
-    while (s[len] != '\0')
-        len++;
+	while (s[len] != '\0')
+	len++;
 
-    while (len)
-        _putchar(s[--len]);
+	while (len)
+	_putchar(s[--len]);
 
-    _putchar('\n');
+	_putchar('\n');
 }
